de04b757eeb1965d3688262baa299fbe7ed1d570
syorito-hatsuki <enderdron3@gmail.com>
2022-11-26 01:15:01 +0200

    Init

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import {Typography} from "@mui/material";
+
+export default function App() {
+    return (
+        <>
+            <Typography variant="h1" component="h2">h1. Heading</Typography>
+        </>
+    )
+}
