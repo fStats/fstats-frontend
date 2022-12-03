6075ab992352c633039362e786c4af5674bc476a
syorito-hatsuki <enderdron3@gmail.com>
2022-12-03 23:13:31 +0200

    Created first test request and tmp view

--- src/pages/ModsList.tsx
+++ src/pages/ModsList.tsx
@@ -1,5 +1,40 @@
-import Typography from "@mui/material/Typography";
+import axios from "axios";
+import {useState} from "react";
+import {Card, CardActionArea, CardContent, Stack, Typography} from "@mui/material";
 
 export function ModsList() {
-    return(<Typography>ModsList</Typography>)
+
+    const requestUrl: string = "https://api.fstats.dev/v1/projects"
+
+    const [projects, setProjects] = useState([Project])
+
+    axios.get(requestUrl)
+        .then(response => {
+            setProjects(response.data);
+        })
+        .catch(error => {
+            console.log("Error *** : " + error);
+        });
+
+    return (
+        <Stack spacing={2} justifyContent="center" padding={4}>
+            {
+                projects.map((data) => (
+                    <Card>
+                        <CardActionArea>
+                            <CardContent>
+                                <Typography>{data.id} | {data.name}</Typography>
+                            </CardContent>
+                        </CardActionArea>
+                    </Card>
+                ))
+            }
+        </Stack>
+    )
+}
+
+let Project: {
+     id: number
+     name: string
+     ownerId: number
 }
