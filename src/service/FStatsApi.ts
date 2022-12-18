fb073a0fdb65cdc8745b5566e7486f202ed3714c
syorito-hatsuki <enderdron3@gmail.com>
2022-12-18 19:23:17 +0200

    Added new API implements

--- src/service/FStatsApi.ts
+++ src/service/FStatsApi.ts
@@ -1,7 +1,5 @@
 import {Project} from "../dto/Project";
 import axios from "axios";
-import {LoaderFunctionArgs} from "react-router-dom";
-import {Metrics} from "../dto/local/Metrics";
 
 let requestUrl = "https://api.fstats.dev/v1/"
 
@@ -9,7 +7,26 @@ export const getAllProjects = async (): Promise<Array<Project>> => {
     return await axios.get(`${requestUrl}projects`).then(response => response.data);
 }
 
-export const getMetricById = async ({params}: LoaderFunctionArgs): Promise<Metrics> => {
-    return await axios.get(`${requestUrl}metrics/${params.id}/all`).then(response => response.data);
+export const getSideMetricById = async (id: number) => {
+    return await axios.get(`${requestUrl}metrics/${id}/side`).then(response => response.data);
 }
 
+export const getMinecraftVersionMetricById = async (id: number) => {
+    return await axios.get(`${requestUrl}metrics/${id}/mcversion`).then(response => response.data);
+}
+
+export const getOnlineModeMetricById = async (id: number) => {
+    return await axios.get(`${requestUrl}metrics/${id}/onlinemode`).then(response => response.data);
+}
+
+export const getModVersionMetricById = async (id: number) => {
+    return await axios.get(`${requestUrl}metrics/${id}/modversion`).then(response => response.data);
+}
+
+export const getOperationSystemMetricById = async (id: number) => {
+    return await axios.get(`${requestUrl}metrics/${id}/os`).then(response => response.data);
+}
+
+export const getLocationMetricById = async (id: number) => {
+    return await axios.get(`${requestUrl}metrics/${id}/location`).then(response => response.data);
+}
