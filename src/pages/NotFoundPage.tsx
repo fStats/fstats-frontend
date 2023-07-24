599b14b3bc80606fa7e9c44d3266bae8189848be
syorito-hatsuki <enderdron3@gmail.com>
2023-07-24 13:48:34 +0300

    Updated NotFoundPage

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,9 +1,18 @@
 import {Typography} from "@mui/material";
+import React from "react";
+import Box from "@mui/material/Box";
 
 export function NotFoundPage() {
     return (
-        <div style={{textAlign: "center"}}>
-            <Typography variant="h1">404 🦆 not found</Typography>
-        </div>
+        <Box style={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            textAlign: "center"
+        }}>
+            <Typography variant="h1">404</Typography>
+            <Typography variant="h3">🦆 not found</Typography>
+        </Box>
     )
 }
