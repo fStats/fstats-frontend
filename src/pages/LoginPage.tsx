cee6a1bdc650343ceef50eec40bd32a6ac847349
syorito-hatsuki <enderdron3@gmail.com>
2023-07-26 22:34:45 +0300

    Added login and registration buttons

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,6 +1,7 @@
 import TextField from '@mui/material/TextField';
-import {Card, Container, Stack} from "@mui/material";
+import {Button, Card, Container, Divider, Stack, Typography} from "@mui/material";
 import React from "react";
+import {Link} from "react-router-dom";
 
 export function LoginPage() {
     return (
@@ -8,8 +9,14 @@ export function LoginPage() {
             <br/>
             <Card>
                 <Stack padding={2} spacing={2}>
+                    <Typography variant="h4" align="center">Log In</Typography>
                     <TextField label="Username" variant="outlined" type="text"/>
                     <TextField label="Password" variant="outlined" type="password"/>
+                    <Stack direction="row" divider={<Divider orientation="vertical" flexItem/>} spacing={2}>
+                        <Button variant="contained" sx={{flexGrow: 9}}>Login</Button>
+                        <Button variant="outlined" sx={{flexGrow: 1}} component={Link}
+                                to="/register">Register</Button>
+                    </Stack>
                 </Stack>
             </Card>
         </Container>
