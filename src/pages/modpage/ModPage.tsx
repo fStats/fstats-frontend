80054db2ddf4f648b23e2c2ab66daf9ad4c768f6
syorito-hatsuki <enderdron3@gmail.com>
2023-07-26 22:36:11 +0300

    Moved to React Query realization and removed side metric

--- src/pages/modpage/ModPage.tsx
+++ src/pages/modpage/ModPage.tsx
@@ -1,65 +1,47 @@
-import {Card, CardContent, Typography} from "@mui/material";
-import Grid2 from "@mui/material/Unstable_Grid2";
-import {
-    getLocationMetricById,
-    getMinecraftVersionMetricById,
-    getModVersionMetricById,
-    getOnlineModeMetricById,
-    getOperationSystemMetricById,
-    getSideMetricById
-} from "../../service/FStatsApi";
-import {useEffect, useState} from "react";
-import {MetricPie} from "./components/MetricPie";
+import {Typography} from "@mui/material";
 import {useLocation, useParams} from "react-router-dom";
+import {useMetricCount} from "../../services/metrics";
+import React from "react";
 import {MetricCard} from "./components/MetricCard";
+import Grid2 from "@mui/material/Unstable_Grid2";
 
 export function ModPage() {
-    const [sides, setSides] = useState([]);
-    const [mcVersions, setMcVersions] = useState([]);
-    const [onlineModes, setOnlineModes] = useState([]);
-    const [modVersions, setModVersions] = useState([]);
-    const [oss, setOss] = useState([]);
-    const [locations, setLocations] = useState([]);
-
     const {modId} = useParams();
     const {title} = useLocation().state
     const id = Number.parseInt(modId!!)
 
-    useEffect(() => {
-        getSideMetricById(id).then(data => setSides(data));
-        getMinecraftVersionMetricById(id).then(data => setMcVersions(data));
-        getOnlineModeMetricById(id).then(data => setOnlineModes(data));
-        getModVersionMetricById(id).then(data => setModVersions(data));
-        getOperationSystemMetricById(id).then(data => setOss(data));
-        getLocationMetricById(id).then(data => setLocations(data));
-    }, []);
+    const {data, status, error} = useMetricCount(id)
+
+    if (status === "loading") return (
+        <Typography variant="h4">Loading...</Typography>
+    )
+
+    if (status === "error") return (
+        <Typography variant="h4">{`Error...${error}`}</Typography>
+    )
 
     return (
         <>
             <Typography variant="h2" textAlign="center" paddingTop={2}>{title}</Typography>
             <Grid2 container spacing={2} padding={4}>
                 <Grid2 xs={1} sm={2} md={4}>
-                    <MetricCard name="Side" data={sides}/>
+                    <MetricCard title="Minecraft Version" metric={data.minecraft_version}/>
                 </Grid2>
 
                 <Grid2 xs={1} sm={2} md={4}>
-                    <MetricCard name="Minecraft Version" data={mcVersions}/>
+                    <MetricCard title="Online Mode" metric={data.online_mode}/>
                 </Grid2>
 
                 <Grid2 xs={1} sm={2} md={4}>
-                    <MetricCard name="Online Mode" data={onlineModes}/>
+                    <MetricCard title="Mod Version" metric={data.mod_version}/>
                 </Grid2>
 
                 <Grid2 xs={1} sm={2} md={4}>
-                    <MetricCard name="Mod Version" data={modVersions}/>
+                    <MetricCard title="Operation System" metric={data.os}/>
                 </Grid2>
 
                 <Grid2 xs={1} sm={2} md={4}>
-                    <MetricCard name="Operation System" data={oss}/>
-                </Grid2>
-
-                <Grid2 xs={1} sm={2} md={4}>
-                    <MetricCard name="Location" data={locations}/>
+                    <MetricCard title="Location" metric={data.location}/>
                 </Grid2>
             </Grid2>
         </>
