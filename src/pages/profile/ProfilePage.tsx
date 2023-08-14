20ab3b66f643c648e5a7d0bd8b4628912077486f
syorito-hatsuki <enderdron3@gmail.com>
2023-08-15 00:47:04 +0300

    Updated profile page and made it private

--- src/pages/profile/ProfilePage.tsx
+++ src/pages/profile/ProfilePage.tsx
@@ -1,27 +1,22 @@
-import React, {useEffect, useState} from "react";
-import {Card, CardContent, SpeedDial, Stack, Typography, CardActionArea} from "@mui/material";
+import React from "react";
+import {Card, CardActionArea, CardContent, CircularProgress, Stack, Typography} from "@mui/material";
 import Grid2 from '@mui/material/Unstable_Grid2';
-import {Add} from "@mui/icons-material";
 import {ProfileImage} from "./components/ProfileImage";
-import {useNavigate, useParams} from "react-router-dom";
-import {getUserProjects} from "../../service/FStatsApi";
-import {Project} from "../../dto/Project";
+import {useNavigate} from "react-router-dom";
+import {useUserProjects} from "../../services/users";
+import {useAuth} from "../../hooks/useAuth";
+import {User} from "../../services/types";
 
 export function ProfilePage() {
-    const [projects, setProjects] = useState([] as Array<Project>);
 
     const navigate = useNavigate()
-    const {username} = useParams();
+    const {token} = useAuth()!!
+    const user: User = JSON.parse(atob(token.split('.')[1]))
 
-    useEffect(() => {
-        if (username != null) {
-            getUserProjects(username)
-                .then(projects => setProjects(projects))
-                .catch(() => {
-                    navigate(`../notfound`)
-                })
-        }
-    }, []);
+    const {data, status, error} = useUserProjects(user.id!!)
+
+    if (status === "error") return (<>{error?.message}</>)
+    if (status === "loading") return (<CircularProgress/>)
 
     return (
         <Grid2 container spacing={2} padding={2}>
@@ -29,30 +24,23 @@ export function ProfilePage() {
                 <Card>
                     <ProfileImage/>
                     <CardContent>
-                        <Typography variant="h5" textAlign="center">{username}</Typography>
+                        <Typography variant="h5" textAlign="center">{user.username}</Typography>
                     </CardContent>
                 </Card>
             </Grid2>
             <Grid2 xs={10}>
                 <Stack spacing={2}>
-                    {projects.map((project: Project) => (
+                    {(data.length > 0) ? data.map(project => (
                         <Card>
-                            <CardActionArea onClick={() => navigate(`//./mods/${project.id}`, {
-                                state: {
-                                    title: project.name
-                                }
-                            })}>
+                            <CardActionArea onClick={() => navigate(`//./mods/${project.id}`)}>
                                 <CardContent>
                                     {project.name}
                                 </CardContent>
                             </CardActionArea>
                         </Card>
-                    ))}
+                    )) : <Typography>No Project found</Typography>}
                 </Stack>
             </Grid2>
-            <SpeedDial ariaLabel="Create project"
-                       sx={{position: 'absolute', bottom: 16, right: 16}}
-                       icon={<Add/>}/>
         </Grid2>
     )
 }
