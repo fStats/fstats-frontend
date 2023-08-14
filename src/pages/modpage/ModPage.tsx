469b9684b30c9a8dc25fcff00bfaf0fca472ad0c
syorito-hatsuki <enderdron3@gmail.com>
2023-08-15 00:50:10 +0300

    (WIP) Updated ModPage

--- src/pages/modpage/ModPage.tsx
+++ src/pages/modpage/ModPage.tsx
@@ -1,49 +1,84 @@
-import {Typography} from "@mui/material";
-import {useLocation, useParams} from "react-router-dom";
+import {CircularProgress, Stack, Typography} from "@mui/material";
+import {useParams} from "react-router-dom";
 import {useMetricCount} from "../../services/metrics";
 import React from "react";
 import {MetricCard} from "./components/MetricCard";
 import Grid2 from "@mui/material/Unstable_Grid2";
+import {DataValue} from "../../services/types";
+import {useProject} from "../../services/projects";
 
 export function ModPage() {
     const {modId} = useParams();
-    const {title} = useLocation().state
     const id = Number.parseInt(modId!!)
 
-    const {data, status, error} = useMetricCount(id)
+    const {data: metrics, status, error} = useMetricCount(id)
+    const {data: project} = useProject(id)
 
     if (status === "loading") return (
-        <Typography variant="h4">Loading...</Typography>
+        <Typography variant="h4">
+            <CircularProgress/>
+        </Typography>
     )
 
     if (status === "error") return (
-        <Typography variant="h4">{`Error...${error}`}</Typography>
+        <Typography variant="h4">
+            {error?.message}
+        </Typography>
     )
 
     return (
         <>
-            <Typography variant="h2" textAlign="center" paddingTop={2}>{title}</Typography>
-            <Grid2 container spacing={2} padding={4}>
-                <Grid2 xs={1} sm={2} md={4}>
-                    <MetricCard title="Minecraft Version" metric={data.minecraft_version}/>
+            <Stack direction="row" textAlign="center" paddingTop={2} alignItems="baseline" justifyContent="center">
+                <Typography variant="h2">{project?.name}</Typography>
+                <span style={{padding: 4}}/>
+                <Typography variant="h4">{`by ${project?.owner.username}`}</Typography>
+            </Stack>
+            <Grid2 container spacing={2} padding={4} justifyContent="center">
+                <Grid2>
+                    <MetricCard title="Minecraft Version" metric={metrics.minecraft_version}/>
                 </Grid2>
 
-                <Grid2 xs={1} sm={2} md={4}>
-                    <MetricCard title="Online Mode" metric={data.online_mode}/>
+                <Grid2>
+                    <MetricCard title="Online Mode" metric={formatOnlineMode(metrics.online_mode)}/>
                 </Grid2>
 
-                <Grid2 xs={1} sm={2} md={4}>
-                    <MetricCard title="Mod Version" metric={data.mod_version}/>
+                <Grid2>
+                    <MetricCard title="Mod Version" metric={metrics.mod_version}/>
                 </Grid2>
 
-                <Grid2 xs={1} sm={2} md={4}>
-                    <MetricCard title="Operation System" metric={data.os}/>
+                <Grid2>
+                    <MetricCard title="Operation System" metric={formatOperationSystem(metrics.os)}/>
                 </Grid2>
 
-                <Grid2 xs={1} sm={2} md={4}>
-                    <MetricCard title="Location" metric={data.location}/>
+                <Grid2>
+                    <MetricCard title="Location" metric={metrics.location}/>
+                </Grid2>
+
+                <Grid2>
+                    <MetricCard title="Fabric API" metric={metrics.fabric_api_version}/>
                 </Grid2>
             </Grid2>
         </>
     )
+}
+
+function formatOnlineMode(data: DataValue) {
+    return Object.fromEntries(
+        Object.entries(data).map(([value, count]) => {
+            if (value === 'true') value = 'Online';
+            if (value === 'false') value = 'Offline';
+            return [value, count];
+        })
+    ) as DataValue;
+}
+
+function formatOperationSystem(data: DataValue) {
+    return Object.fromEntries(
+        Object.entries(data).map(([value, count]) => {
+            if (value === 'l') value = 'Linux'
+            if (value === 'm') value = 'MacOS'
+            if (value === 'w') value = 'Windows'
+            return [value, count];
+        })
+    ) as DataValue;
 }
