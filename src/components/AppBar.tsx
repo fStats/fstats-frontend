0bb1979c8c1ea65a7e1cf902d5d59ba74320d4cf
syorito-hatsuki <enderdron3@gmail.com>
2023-08-15 00:50:53 +0300

    Added some components that should be available only after sing in

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -1,19 +1,20 @@
 import * as React from 'react';
-import {useState} from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import AccountCircle from '@mui/icons-material/AccountCircle';
-import {Button, Container} from "@mui/material";
-import {Login} from "@mui/icons-material";
-import {Link, useNavigate} from "react-router-dom";
+import {Avatar, Button, Container, SpeedDial} from "@mui/material";
+import {Add, Login} from "@mui/icons-material";
+import {Link} from "react-router-dom";
+import {useAuth} from "../hooks/useAuth";
+import {User} from "../services/types";
 
 export default function PrimaryAppBar() {
 
-    const [isLogged, setLogged] = useState(false);
-    const navigate = useNavigate()
+    const {token, isAuthorized} = useAuth()!!
+    let user
+    if (isAuthorized) user = JSON.parse(atob(token.split('.')[1])) as User
 
     return (
         <Box sx={{flexGrow: 1}}>
@@ -29,6 +30,8 @@ export default function PrimaryAppBar() {
                             sx={{display: {xs: 'none', sm: 'block'}, textDecoration: "none"}}>
                             Fabric Stats
                         </Typography>
+                        <Typography variant="caption" paddingTop={3}
+                                    sx={{display: {xs: 'none', sm: 'block'}}}>ALPHA</Typography>
                         <Box sx={{flexGrow: 1}}/>
                         <Button key={1} sx={{my: 2, color: 'white', display: 'block'}} component={Link} to={"/random"}>Random
                             mod</Button>
@@ -36,17 +39,11 @@ export default function PrimaryAppBar() {
                             list</Button>
                         <Button key={1} sx={{my: 2, color: 'white', display: 'block'}} component={Link} to={"/global"}>Global
                             stats</Button>
-                        {isLogged ?
-                            <IconButton
-                                size="large"
-                                edge="end"
-                                aria-haspopup="true"
-                                color="inherit"
-                                component={Link}
-                                to={"/profile"}>
-                                <AccountCircle/>
-                            </IconButton>
-                            :
+                        {isAuthorized ?
+                            <Avatar style={{textDecoration: "none"}}
+                                    component={Link}
+                                    to={"/profile"}>{user?.username[0]}
+                            </Avatar> :
                             <IconButton
                                 size="large"
                                 edge="end"
@@ -60,6 +57,11 @@ export default function PrimaryAppBar() {
                     </Toolbar>
                 </Container>
             </AppBar>
+            {isAuthorized ? <SpeedDial
+                    ariaLabel="Create project"
+                    sx={{position: 'absolute', bottom: 16, right: 16}}
+                    icon={<Add/>}/>
+                : <></>}
         </Box>
     );
 }
