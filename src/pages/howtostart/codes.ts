b06a549f2abdbdaef241d9a6a09a0a8cc4b2f009
syorito-hatsuki <enderdron3@gmail.com>
2023-10-31 14:05:55 +0200

    Updated codes.ts

--- src/pages/howtostart/codes.ts
+++ src/pages/howtostart/codes.ts
@@ -4,10 +4,10 @@ export const kotlinGradleCode: string = `repositories {
 
 dependencies {
     // Option 1: Include fStats API to project for it available within your own jar IT'S ONLY ~9KB!
-    include(modImplementation("maven.modrinth", "fstatsapi", "<version>"))
+    include(modImplementation("maven.modrinth", "fstats", "<version>"))
     
     // Option 2: Depend on fStats API, but require that users install it manually
-    modImplementation("maven.modrinth", "fstatsapi", "<version>")
+    modImplementation("maven.modrinth", "fstats", "<version>")
 }`
 
 export const javaGradleCode: string = `repositories {
@@ -18,14 +18,14 @@ export const javaGradleCode: string = `repositories {
 
 dependencies {
     // Option 1: Include fStats API to project for it to be available within your own jar (IT'S ONLY ~9KB!)
-    include(modImplementation("maven.modrinth:fstatsapi:<version>")
+    include(modImplementation("maven.modrinth:fstats:<version>")
     
     // Option 2: Depend on fStats API, but require that users install it manually
-    modImplementation "maven.modrinth:fstatsapi:<version>"
+    modImplementation "maven.modrinth:fstats:<version>"
 }`
 
 export const fabricJsonSuggests: string = `"suggests": {
-    "fstatsapi": "*"
+    "fstats-api": "*"
 }`
 
 export const fabricJsonSetup: string = `"custom": {
