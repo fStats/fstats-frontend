c1ad904f0fd89ae10963d19191551dd5598c8e2a
syorito-hatsuki <enderdron3@gmail.com>
2024-06-20 02:35:20 +0300

    Added name convert for a new type of location

--- src/pages/project/components/MetricCard.tsx
+++ src/pages/project/components/MetricCard.tsx
@@ -2,20 +2,29 @@ import {Card, CardContent, Typography} from "@mui/material";
 import {CardProps} from "./types";
 import {Pie} from "react-chartjs-2";
 import {colors} from "./colors";
+import {iso2name} from "../../../mics/countryConvert.ts";
+import {DataValue} from "../../../services/types.ts";
 
 export default function MetricCard(props: CardProps) {
-    return Object.keys(props.metric).length > 0 ? (
+
+    let metric = props.metric
+
+    if (props.title === "Location") {
+        metric = (Object.fromEntries(Object.entries(metric).map(([value, count]) => [iso2name[value], count])) as DataValue)
+    }
+
+    return Object.keys(metric).length > 0 ? (
         <Card>
             <CardContent>
                 <Typography variant="h6" textAlign="center">{props.title}</Typography>
                 <Pie data={{
                     datasets: [{
                         label: 'Count',
-                        data: Object.values(props.metric) as number[],
+                        data: Object.values(metric) as number[],
                         borderWidth: 1,
                         backgroundColor: colors,
                     }],
-                    labels: Object.keys(props.metric) as string[],
+                    labels: Object.keys(metric) as string[],
                 }} options={{
                     radius: "90%",
                     plugins: {
