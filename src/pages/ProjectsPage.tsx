68200d632a1407580bf8efcd5ad5dbb5e88614ab
syorito-hatsuki <enderdron3@gmail.com>
2024-06-30 18:32:59 +0300

    Moved search bar into pagination

--- src/pages/ProjectsPage.tsx
+++ src/pages/ProjectsPage.tsx
@@ -1,5 +1,6 @@
 import {
     Paper,
+    Stack,
     Table,
     TableBody,
     TableCell,
@@ -7,6 +8,7 @@ import {
     TableHead,
     TablePagination,
     TableRow,
+    TextField,
     Typography
 } from "@mui/material";
 import {useProjects} from "../services/projects"
@@ -14,17 +16,22 @@ import {Loader} from "../components/Loader"
 import {useNavigate, useSearchParams} from "react-router-dom";
 import {useLabel} from "../hooks/useLabel";
 import {useSnackbar} from "notistack";
+import {useState} from "react";
+import {Project} from "../services/types.ts";
 
 export default function ProjectsPage() {
 
     useLabel()?.setLabel("Projects catalogue")
 
     const [searchParams, setSearchParams] = useSearchParams();
+    const [searchFilter, setSearchFilter] = useState("")
     const {data, status, error} = useProjects()
     const navigate = useNavigate()
     const {enqueueSnackbar} = useSnackbar();
 
     const page = Number.parseInt(searchParams.get('page') || "0") || 0;
+
+    const emptyRows = page > 0 ? Math.max(0, (1 + page) * 10 - (data?.length ?? 0)) : 0;
 
     if (status === "loading") return <Loader/>
 
@@ -34,11 +41,13 @@ export default function ProjectsPage() {
         return <></>
     }
 
+    const filteredData = (): Project[] => searchFilter.length > 0 ? data.filter((value) => value.name.toLowerCase().includes(searchFilter.toLowerCase())) : data
+
     return (
         <>
             {data.length > 0 ?
                 <Paper sx={{width: '100%', overflow: 'hidden'}}>
-                    <TableContainer sx={{maxHeight: 600}}>
+                    <TableContainer>
                         <Table stickyHeader aria-label="sticky table">
                             <TableHead>
                                 <TableRow>
@@ -47,23 +56,34 @@ export default function ProjectsPage() {
                                 </TableRow>
                             </TableHead>
                             <TableBody>
-                                {data.slice(page * 10, page * 10 + 10).map((row) =>
+                                {(10 > 0 ? filteredData().slice(page * 10, page * 10 + 10) : filteredData()).map((row) => (
                                     <TableRow hover tabIndex={-1} key={row.name}
                                               onClick={() => navigate(`/project/${row.id}`)}>
                                         <TableCell>{row.name}</TableCell>
                                         <TableCell>{row.owner?.username}</TableCell>
-                                    </TableRow>)}
+                                    </TableRow>
+                                ))}
+                                {emptyRows > 0 && (
+                                    <TableRow style={{height: 53 * emptyRows}}>
+                                        <TableCell colSpan={6}/>
+                                    </TableRow>
+                                )}
                             </TableBody>
                         </Table>
                     </TableContainer>
-                    <TablePagination
-                        component="div"
-                        rowsPerPage={10}
-                        count={data.length}
-                        page={page}
-                        onPageChange={(_, newPage) => setSearchParams({page: newPage.toString()})}
-                        rowsPerPageOptions={[]}
-                    />
+                    <Stack direction="row" alignItems="center">
+                        <TextField sx={{flexGrow: 1}} variant="outlined" placeholder="Enter project name"
+                                   value={searchFilter}
+                                   onChange={event => setSearchFilter(event.target.value)}/>
+                        <TablePagination
+                            component="div"
+                            rowsPerPage={10}
+                            count={filteredData().length}
+                            page={page}
+                            onPageChange={(_, newPage) => setSearchParams({page: newPage.toString()})}
+                            rowsPerPageOptions={[]}
+                        />
+                    </Stack>
                 </Paper> : <Typography variant="h4" textAlign="center">No project available</Typography>}
         </>
     )
