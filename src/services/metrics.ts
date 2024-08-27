930808c98a5da1d7ec4015229b3e191352961606
syorito-hatsuki <enderdron3@gmail.com>
2024-08-28 00:53:41 +0300

    Updated line and pie request hooks

--- src/services/metrics.ts
+++ src/services/metrics.ts
@@ -3,12 +3,12 @@ import {useQuery} from "@tanstack/react-query";
 import {LineMetric, PieMetric} from "./types";
 import {Mode} from "../pages/project/components/card/TimelineCard.tsx";
 
-export const useLineMetricMutation = (projectId: number, from: number, mode: Mode) => useQuery<LineMetric, Error>({
-    queryKey: ["metricLine", projectId, mode],
-    queryFn: () => getLineMetric(projectId, from).then(value => value),
+export const useLineMetricMutation = (projectId: number, from: number, mode: Mode, serverSide: boolean) => useQuery<LineMetric, Error>({
+    queryKey: ["metricLine", projectId, mode, serverSide],
+    queryFn: () => getLineMetric(projectId, from, serverSide).then(value => value),
 })
 
-export const usePieMetric = (projectId: number) => useQuery<PieMetric, Error>({
-    queryKey: ["metricPie", projectId],
-    queryFn: () => getPieMetric(projectId)
+export const usePieMetric = (projectId: number, serverSide: boolean) => useQuery<PieMetric, Error>({
+    queryKey: ["metricPie", projectId, serverSide],
+    queryFn: () => getPieMetric(projectId, serverSide)
 })
