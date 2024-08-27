1665fca0bc321b762273401744bb43e19d2e47c4
syorito-hatsuki <enderdron3@gmail.com>
2024-08-27 19:01:53 +0300

    Updated line and pie request

--- src/services/fStatsApi.ts
+++ src/services/fStatsApi.ts
@@ -111,17 +111,17 @@ export const removeProjectFromFavorite = async (projectId: number, token: string
 }
 
 /*   METRICS   */
-export const getLineMetric = async (projectId: number, from: number): Promise<LineMetric> => {
+export const getLineMetric = async (projectId: number, from: number, serverSide: boolean): Promise<LineMetric> => {
     console.log(from)
-    const response = await fetch(`${hostUrl}/metrics/${projectId}/line${(from !== undefined && from > 0) ? `?from=${from / 1000}` : ""}`)
+    const response = await fetch(`${hostUrl}/metrics/${projectId}/line?server_side=${serverSide}${(from !== undefined && from > 0) ? `&from=${from / 1000}` : ""}`)
 
     if (response.status !== 200) throw new Error((await response.json() as ApiMessage).message)
 
     return await response.json() as LineMetric
 }
 
-export const getPieMetric = async (projectId: number): Promise<PieMetric> => {
-    const response = await fetch(`${hostUrl}/metrics/${projectId}/pie`)
+export const getPieMetric = async (projectId: number, serverSide: boolean): Promise<PieMetric> => {
+    const response = await fetch(`${hostUrl}/metrics/${projectId}/pie?server_side=${serverSide}`)
 
     if (response.status !== 200) throw new Error((await response.json() as ApiMessage).message)
 
