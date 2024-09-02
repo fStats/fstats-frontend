9bcdb45cda08d3a50c747f4076e010ef00fc63c2
syorito-hatsuki <enderdron3@gmail.com>
2024-09-02 23:33:51 +0300

    Added user modify request

--- src/services/fStatsApi.ts
+++ src/services/fStatsApi.ts
@@ -31,6 +31,18 @@ export const getUserFavorites = async (userId: number, token: string): Promise<P
     return await response.json() as Project[]
 }
 
+export const patchUser = async (token: string, user: User): Promise<ApiMessage> => {
+    const response = await fetch(`${hostUrl}/users`, {
+        method: "PATCH", headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`
+        }, body: JSON.stringify(user)
+    })
+
+    if (response.status !== 202) throw new Error((await response.json() as ApiMessage).message)
+
+    return await response.json() as ApiMessage
+}
 
 export const deleteUser = async (token: string): Promise<ApiMessage> => {
     const response = await fetch(`${hostUrl}/users`, {
