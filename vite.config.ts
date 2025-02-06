48594ce9a244285050a70a3e103e727db0d83c25
syorito-hatsuki <enderdron3@gmail.com>
2025-02-06 11:08:52 +0200

    Removed unused domain base

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-    base: 'https://syorito-hatsuki.github.io/',
+    base: '/',
     plugins: [react()]
 })
