a1367db056d4d3b489b08c71fb546095072995b8
syorito-hatsuki <enderdron3@gmail.com>
2025-04-17 12:22:02 +0300

    Added alternative view for location

--- src/pages/project/components/card/WorldMapCard.tsx
+++ src/pages/project/components/card/WorldMapCard.tsx
@@ -1,13 +1,28 @@
 import {Chart} from 'react-chartjs-2';
 import {Feature} from "chartjs-chart-geo";
-import {Card, CardContent, Typography} from "@mui/material";
+import {
+    Avatar,
+    Card,
+    CardContent,
+    IconButton,
+    List,
+    ListItem,
+    ListItemAvatar,
+    ListItemText,
+    Stack,
+    Typography
+} from "@mui/material";
 import {DataValue} from "../../../../services/types.ts";
 import {useWorldMap} from "../../../../services/worldatlas/worldMap.ts";
 import {CardProps} from "./types.ts";
-import {iso2code} from "../../../../mics/convertor/country.ts";
+import {code2shortIso, iso2code} from "../../../../mics/convertor/country.ts";
 import {mergeClientAndServerData} from "../../../../mics/merge.ts";
+import {useState} from "react";
+import {FormatListBulleted, Map} from "@mui/icons-material";
 
 export function WorldMapCard(props: CardProps) {
+
+    const [listMode, setListMode] = useState(false);
 
     const {data, status} = useWorldMap()
 
@@ -27,42 +42,96 @@ export function WorldMapCard(props: CardProps) {
     return (
         status === "success" && <Card>
             <CardContent>
-                <Typography variant="h6" textAlign="center">{props.title}</Typography>
-                <Chart
-                    type="choropleth"
-                    data={{
-                        labels: (data as Feature[]).map(d => d.properties.name),
-                        datasets: [{
-                            data: (data as Feature[]).map(d => ({feature: d, value: metric[d.id] ?? 0})),
-                            backgroundColor: (ctx, _) =>
-                                gradientColor((ctx.chart.data.datasets[0].data[ctx.dataIndex] as {
-                                    value: number
-                                })?.value, false)
-                        }]
-                    }}
-                    options={{
-                        plugins: {
-                            datalabels: {
-                                display: false
-                            }
+                <Stack direction="row" spacing={2}>
+                    <Typography flexGrow={1} paddingLeft={8} variant="h6"
+                                textAlign="center">{props.title}</Typography>
+                    <IconButton onClick={() => setListMode((prev) => !prev)}>
+                        {listMode ? <Map/> : <FormatListBulleted/>}
+                    </IconButton>
+                </Stack>
+                {listMode ?
+                    <List sx={{
+                        overflow: 'auto',
+                        maxHeight: 380,
+                        position: 'relative',
+                        '&::-webkit-scrollbar': {
+                            width: 8,
                         },
-                        scales: {
-                            projection: {
-                                axis: 'x',
-                                projection: 'equalEarth'
-                            },
-                            color: {
-                                interpolate: (v) => gradientColor(v, true),
-                                max: maxValue,
-                                axis: "x",
-                                ticks: {
-                                    precision: 0,
+                        '&::-webkit-scrollbar-track': {
+                            backgroundColor: 'transparent',
+                        },
+                        '&::-webkit-scrollbar-button': {
+                            display: 'none',
+                            width: 0,
+                            height: 0,
+                        },
+                        '&::-webkit-scrollbar-thumb': {
+                            backgroundColor: (theme) =>
+                                theme.palette.mode === 'light'
+                                    ? theme.palette.grey[400]
+                                    : theme.palette.grey[700],
+                            borderRadius: 4,
+                        },
+                        '&::-webkit-scrollbar-thumb:hover': {
+                            backgroundColor: (theme) =>
+                                theme.palette.mode === 'light'
+                                    ? theme.palette.grey[500]
+                                    : theme.palette.grey[600],
+                        },
+                        scrollbarWidth: 'thin',
+                        scrollbarColor: (theme) => `${theme.palette.grey[500]} transparent`,
+                    }}>
+                        {(data as Feature[]).filter(d => metric[d.id])
+                            .sort((a: Feature, b: Feature) => metric[b.id] - metric[a.id])
+                            .map((d: Feature) =>
+                                <ListItem disablePadding>
+                                    <ListItemAvatar>
+                                        <Avatar variant="square" alt={code2shortIso[d.id]}
+                                                src={`https://flagsapi.com/${code2shortIso[d.id]}/flat/64.png`}/>
+                                    </ListItemAvatar>
+                                    <ListItemText primary={d.properties.name} secondary={metric[d.id]}/>
+                                </ListItem>
+                            )}
+                    </List>
+                    :
+                    <Chart
+                        type="choropleth"
+                        data={{
+                            labels: (data as Feature[]).map(d => d.properties.name),
+                            datasets: [{
+                                data: (data as Feature[]).map(d => ({feature: d, value: metric[d.id] ?? 0})),
+                                backgroundColor: (ctx, _) =>
+                                    gradientColor((ctx.chart.data.datasets[0].data[ctx.dataIndex] as {
+                                        value: number
+                                    })?.value, false)
+                            }]
+                        }}
+                        options={{
+                            plugins: {
+                                legend: {
+                                    display: false,
                                 },
-                                beginAtZero: true
+                                datalabels: {
+                                    display: false
+                                }
+                            },
+                            scales: {
+                                projection: {
+                                    axis: 'x',
+                                    projection: 'equalEarth'
+                                },
+                                color: {
+                                    interpolate: (v) => gradientColor(v, true),
+                                    max: maxValue,
+                                    axis: "x",
+                                    ticks: {
+                                        precision: 0,
+                                    },
+                                    beginAtZero: true
+                                }
                             }
-                        }
-                    }}
-                />
+                        }}
+                    />}
             </CardContent>
         </Card>
     )
