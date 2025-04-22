9f713da82279e5cb8ac8b003790ee4adf4091eb4
syorito-hatsuki <enderdron3@gmail.com>
2025-04-22 10:38:38 +0300

    Improved formatters

--- src/mics/formatter/onlineMode.ts
+++ src/mics/formatter/onlineMode.ts
@@ -1,9 +1,11 @@
 import {DataValue} from "../../services/types.ts";
+import {KeyValue} from "../convertor/types.ts";
 
-export const formatOnlineMode = (data: DataValue) => Object.fromEntries(
-    Object.entries(data).map(([value, count]) => {
-        if (value === "true") value = "Online";
-        else if (value === "false") value = "Offline";
-        return [value, count];
-    })
-) as DataValue
+const onlineModeMap: KeyValue = {
+    "true": "Online",
+    "false": "Offline",
+};
+
+export const formatOnlineMode = (data: DataValue): DataValue => Object.fromEntries(
+    Object.entries(data).map(([key, count]) => [onlineModeMap[key] ?? key, count])
+);
--- src/mics/formatter/operationSystem.ts
+++ src/mics/formatter/operationSystem.ts
@@ -1,11 +1,13 @@
 import {DataValue} from "../../services/types.ts";
+import {KeyValue} from "../convertor/types.ts";
+
+const osMap: KeyValue = {
+    "l": "Linux",
+    "m": "MacOS",
+    "w": "Windows",
+    "o": "Other",
+};
 
 export const formatOperationSystem = (data: DataValue) => Object.fromEntries(
-    Object.entries(data).map(([value, count]) => {
-        if (value === 'l') value = "Linux"
-        else if (value === 'm') value = "MacOS"
-        else if (value === 'w') value = "Windows"
-        else if (value === 'o') value = "Other"
-        return [value, count];
-    })
-) as DataValue
+    Object.entries(data).map(([value, count]) => [osMap[value] ?? value, count])
+) as DataValue;
