4880d4845c0ac3a6eb44fe710f3e94d5f9ce449a
syorito-hatsuki <enderdron3@gmail.com>
2025-05-25 01:08:15 +0300

    Changed styles of Shutdown page

--- src/pages/ShutdownPage.tsx
+++ src/pages/ShutdownPage.tsx
@@ -1,13 +1,13 @@
 import {
-    TimelineOppositeContent as BaseTimelineOppositeContent,
     Timeline,
     TimelineConnector,
     TimelineContent,
     TimelineDot,
     TimelineItem,
+    TimelineOppositeContent,
     TimelineSeparator
 } from "@mui/lab";
-import {Box, styled, Typography} from "@mui/material";
+import {Box, CssBaseline, Typography} from "@mui/material";
 
 import {Loader} from "@components/Loader";
 import {useNews} from "@services/news/news";
@@ -16,32 +16,31 @@ export default function ShutdownPage() {
 
     const {data, status, error} = useNews()
 
-    const TimelineOppositeContent = styled(BaseTimelineOppositeContent)({
-        flex: 0.1,
-    });
-
-    return <>
-        <Box flexGrow={1} padding={2} textAlign="center">
-            <Typography variant="h2">Service temporary shutdown</Typography>
-            <Typography variant="h4" paddingTop={2}>News / Updates</Typography>
+    return (
+        <Box padding={4}>
+            <CssBaseline/>
+            <Box flexGrow={1} padding={2} textAlign="center">
+                <Typography variant="h2">Service temporary shutdown</Typography>
+                <Typography variant="h4" paddingTop={2}>News / Updates</Typography>
+            </Box>
+            {status === "pending" && <Loader/>}
+            {error && <Typography variant="body2" paddingTop={2}>{error.message}</Typography>}
+            {data && <Timeline>
+                {data.map((item, index) =>
+                    <TimelineItem id={String(index)}>
+                        <TimelineOppositeContent color="textSecondary" sx={{flex: 0.1}}>
+                            {new Date(item.date * 1000).toLocaleString()}
+                        </TimelineOppositeContent>
+                        <TimelineSeparator>
+                            <TimelineDot/>
+                            <TimelineConnector/>
+                        </TimelineSeparator>
+                        <TimelineContent>
+                            <Typography>{item.text}</Typography>
+                        </TimelineContent>
+                    </TimelineItem>
+                )}
+            </Timeline>}
         </Box>
-        {status && <Loader/>}
-        {error && <Typography variant="body2" paddingTop={2}>{error.message}</Typography>}
-        {data && <Timeline>
-            {data.map((item, index) =>
-                <TimelineItem id={String(index)}>
-                    <TimelineOppositeContent color="textSecondary">
-                        {new Date(item.date * 1000).toLocaleString()}
-                    </TimelineOppositeContent>
-                    <TimelineSeparator>
-                        <TimelineDot/>
-                        <TimelineConnector/>
-                    </TimelineSeparator>
-                    <TimelineContent>
-                        <Typography>{item.text}</Typography>
-                    </TimelineContent>
-                </TimelineItem>
-            )}
-        </Timeline>}
-    </>
+    )
 }
