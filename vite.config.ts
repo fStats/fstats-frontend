fce2e324eed05be676b849d5b028ec492595b89a
syorito-hatsuki <enderdron3@gmail.com>
2025-05-30 18:47:14 +0300

    Improved vite config with chunking and analyzer

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,53 @@
-import {defineConfig} from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import {defineConfig} from "vite"
+import react from "@vitejs/plugin-react-swc"
 import tsconfigPaths from "vite-tsconfig-paths";
+import {visualizer} from "rollup-plugin-visualizer";
 
-export default defineConfig({
-    base: "/",
-    plugins: [
-        react(),
-        tsconfigPaths(),
-    ]
+export default defineConfig(({mode}) => {
+    const shouldAnalyze = mode === 'analyze';
+
+    return {
+        base: "/",
+        build: {
+            rollupOptions: {
+                output: {
+                    manualChunks: {
+                        vendor: [
+                            "react",
+                            "react-dom",
+                            "@tanstack/react-query",
+                            "react-router-dom",
+                            "date-fns",
+                            "notistack",
+                        ],
+                        material: [
+                            "@mui/material",
+                            "@mui/lab",
+                            "@emotion/react",
+                            "@emotion/styled",
+                        ],
+                        muiIcons: ["@mui/icons-material"],
+                        charts: [
+                            "chart.js",
+                            "react-chartjs-2",
+                            "chartjs-adapter-date-fns",
+                            "chartjs-chart-geo",
+                            "chartjs-plugin-datalabels",
+                            "chartjs-plugin-zoom",
+                        ],
+                        syntaxHighlighter: ["react-syntax-highlighter"],
+                    },
+                },
+            },
+        },
+        plugins: [
+            react(),
+            tsconfigPaths(),
+            shouldAnalyze && visualizer({
+                open: true,
+                gzipSize: true,
+                brotliSize: true,
+            }),
+        ].filter(Boolean)
+    }
 })
