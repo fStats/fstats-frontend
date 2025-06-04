8a3591d9a2a6758543bbdf4f05f9a172dd0fc240
syorito-hatsuki <enderdron3@gmail.com>
2025-06-05 02:52:17 +0300

    Replaced gradient generating with utility method

--- src/pages/project/components/card/WorldMapCard.tsx
+++ src/pages/project/components/card/WorldMapCard.tsx
@@ -17,6 +17,7 @@ import {Chart} from "react-chartjs-2";
 
 import {DataValue} from "@services/fstats/types";
 import {useWorldMap} from "@services/worldatlas/worldMap";
+import {getGradientColor} from "@utils/color";
 import {code2shortIso, iso2code} from "@utils/convertors/country";
 import {mergeClientAndServerData} from "@utils/merge";
 
@@ -36,11 +37,6 @@ export function WorldMapCard(props: CardProps) {
     const values = Object.values(metric);
     const minValue = Math.min(...values);
     const maxValue = Math.max(...values);
-
-    const gradientColor = (value: number, skipOffset: boolean): string => {
-        const offset = skipOffset ? value : Math.max(0, Math.min(1, (value - minValue) / (maxValue - minValue)));
-        return `rgb(${Math.round(5 + 28 * offset)}, ${Math.round(5 + 186 * offset)}, ${Math.round(5 + 95 * offset)})`;
-    };
 
     return (
         status === "success" && <Card>
@@ -104,9 +100,9 @@ export function WorldMapCard(props: CardProps) {
                             datasets: [{
                                 data: (data as Feature[]).map(d => ({feature: d, value: metric[d.id] ?? 0})),
                                 backgroundColor: (ctx) =>
-                                    gradientColor((ctx.chart.data.datasets[0].data[ctx.dataIndex] as {
+                                    getGradientColor((ctx.chart.data.datasets[0].data[ctx.dataIndex] as {
                                         value: number
-                                    })?.value, false)
+                                    })?.value, minValue, maxValue, false)
                             }]
                         }}
                         options={{
@@ -124,7 +120,7 @@ export function WorldMapCard(props: CardProps) {
                                     projection: "equalEarth"
                                 },
                                 color: {
-                                    interpolate: (v) => gradientColor(v, true),
+                                    interpolate: (v) => getGradientColor(v, minValue, maxValue, true),
                                     max: maxValue,
                                     axis: "x",
                                     ticks: {
