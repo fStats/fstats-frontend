4d219bd5bd7fe3b9ce6c0366701cac58a4a55b3a
syorito-hatsuki <enderdron3@gmail.com>
2025-06-07 18:18:52 +0300

    Improved homepage on mobile

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,5 @@
 import {ArrowForward} from "@mui/icons-material";
-import {Button, Card, CardActionArea, CardContent, Stack, Typography} from "@mui/material";
+import {Button, Card, CardActionArea, CardContent, Grid, Typography} from "@mui/material";
 import {Link} from "react-router-dom";
 
 import {useLabel} from "@hooks/useLabel";
@@ -8,78 +8,100 @@ export default function HomePage() {
 
     useLabel()?.setLabel("Home");
 
+    const cards = [
+        {
+            title: "Fabric Metrics",
+            description: (
+                <>
+                    fStats allows you to <b>collect usage data</b> of your mods.
+                </>
+            ),
+            to: "project/1",
+        },
+        {
+            title: "Open Source",
+            description: (
+                <>
+                    fStats is <b>completely open source</b>! Feel free to check out our GitHub.
+                </>
+            ),
+            to: "https://github.com/fStats",
+        },
+        {
+            title: "Plug and Play",
+            description: (
+                <>
+                    <b>Simple</b> to implement <b>into already done</b> project. Just embed it to mod and{" "}
+                    <b>add 1 row into fabric.mod.json</b>.
+                </>
+            ),
+            to: "getting-started",
+        },
+        {
+            title: "OpenAPI",
+            description: (
+                <>
+                    The <b>REST-Api</b> of fStats is very simple. It's meant to allow you to{" "}
+                    <b>make your own service</b> based on fStats data.
+                </>
+            ),
+            to: "https://api.fstats.dev/swagger",
+        },
+        {
+            title: "Join our Discord",
+            description: (
+                <>
+                    Keep <b>up-to-date</b> with the development of fStats, <b>get support</b> or{" "}
+                    <b>just chat</b>.
+                </>
+            ),
+            to: "https://discord.gg/pbwnMwnUD6",
+        },
+    ];
+
     return (
-        <Stack spacing={8} paddingTop={8} paddingX={16}>
+        <Grid container spacing={8} paddingTop={8} paddingX={{ xs: 2, md: 16 }} direction="column">
+            <Grid>
+                <Grid container direction="column" spacing={4} alignItems="center">
+                    <Grid>
+                        <Typography variant="h3" textAlign="center">
+                            fStats - Fabric mods metric system
+                        </Typography>
+                    </Grid>
+                    <Grid>
+                        <Button
+                            color="success"
+                            variant="contained"
+                            component={Link}
+                            to="getting-started"
+                            endIcon={<ArrowForward />}
+                        >
+                            Getting started
+                        </Button>
+                    </Grid>
+                </Grid>
+            </Grid>
 
-            <Stack alignItems="center" spacing={8}>
-                <Typography variant="h3" textAlign="center">fStats - Fabric mods metric system</Typography>
-                <Button color="success" variant="contained" component={Link} to="getting-started" endIcon={
-                    <ArrowForward/>
-                }>Getting start</Button>
-            </Stack>
-
-            <Stack spacing={2}>
-                <Stack direction="row" spacing={2}>
-                    <Card style={{flexBasis: "100%"}}>
-                        <CardActionArea component={Link} to="project/1">
-                            <CardContent>
-                                <Typography variant="h5" textAlign="center" paddingBottom={1}>Fabric
-                                    Metrics</Typography>
-                                <Typography variant="body1">
-                                    fStats allows you to <b>collect usage data</b> of your mods.
-                                </Typography>
-                            </CardContent>
-                        </CardActionArea>
-                    </Card>
-                    <Card style={{flexBasis: "100%"}}>
-                        <CardActionArea component={Link} to="https://github.com/fStats">
-                            <CardContent>
-                                <Typography variant="h5" textAlign="center" paddingBottom={1}>Open Source</Typography>
-                                <Typography variant="body1">
-                                    fStats is <b>completely open source</b>! Feel free to check out our GitHub.
-                                </Typography>
-                            </CardContent>
-                        </CardActionArea>
-                    </Card>
-                </Stack>
-                <Stack direction="row" spacing={2}>
-                    <Card style={{flexBasis: "100%"}}>
-                        <CardActionArea component={Link} to="getting-started">
-                            <CardContent>
-                                <Typography variant="h5" textAlign="center" paddingBottom={1}>Plug and Play</Typography>
-                                <Typography variant="body1">
-                                    <b>Simple</b> to implement <b>into already done</b> project.
-                                    Just embed it to mod and <b>add 1 row into fabric.mod.json</b>.
-                                </Typography>
-                            </CardContent>
-                        </CardActionArea>
-                    </Card>
-                    <Card style={{flexBasis: "100%"}}>
-                        <CardActionArea component={Link} to="https://api.fstats.dev/swagger">
-                            <CardContent>
-                                <Typography variant="h5" textAlign="center" paddingBottom={1}>OpenAPI</Typography>
-                                <Typography variant="body1">
-                                    The <b>REST-Api</b> of fStats is very simple.
-                                    It's meant to allow you to <b>make our own service</b> based on fStats data and
-                                    etc.
-                                </Typography>
-                            </CardContent>
-                        </CardActionArea>
-                    </Card>
-                    <Card style={{flexBasis: "100%"}}>
-                        <CardActionArea component={Link} to="https://discord.gg/pbwnMwnUD6">
-                            <CardContent>
-                                <Typography variant="h5" textAlign="center" paddingBottom={1}>Join our
-                                    Discord</Typography>
-                                <Typography variant="body1">
-                                    Keep <b>up-to-date</b> with the development of fStats,
-                                    <b>get support</b> or <b>just chat</b>.
-                                </Typography>
-                            </CardContent>
-                        </CardActionArea>
-                    </Card>
-                </Stack>
-            </Stack>
-        </Stack>
+            <Grid>
+                <Grid container spacing={2} justifyContent="center">
+                    {cards.map(card => (
+                        <Grid size={{xs:12, sm:6, md:4}} key={card.title}>
+                            <Card sx={{ height: "100%" }}>
+                                <CardActionArea component={Link} to={card.to} sx={{ height: "100%" }}>
+                                    <CardContent>
+                                        <Typography variant="h5" textAlign="center" pb={1}>
+                                            {card.title}
+                                        </Typography>
+                                        <Typography variant="body1">
+                                            {card.description}
+                                        </Typography>
+                                    </CardContent>
+                                </CardActionArea>
+                            </Card>
+                        </Grid>
+                    ))}
+                </Grid>
+            </Grid>
+        </Grid>
     );
 }
