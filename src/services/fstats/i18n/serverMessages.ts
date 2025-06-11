8cb2cc36e2e59bcd56ece5800aa594a4ce0e3d62
syorito-hatsuki <enderdron3@gmail.com>
2025-06-12 01:15:34 +0300

    Moved all server related translates to the same scope

--- src/services/fstats/i18n/serverMessages.ts
+++ src/services/fstats/i18n/serverMessages.ts
@@ -1,21 +1,21 @@
 import {t} from "i18next";
 
 export const serverMessageKeyMap: Record<string, string> = {
-    "Incorrect username or password": "page.login.server.incorrect",
-    "Username or password not match requirements": "page.register.server.notmatchrequirements",
-    "User created": "page.register.server.created",
-    "Username already exist": "page.register.server.exist",
-    "Project ID must be number": "page.profile.server.incorrectid",
-    "User not found": "page.profile.server.usernotfound",
-    "Project name can't be empty": "page.profile.dialog.create.server.empty",
-    "Project name doesn't match requirements": "page.profile.dialog.create.server.notmatch",
-    "Something went wrong": "page.server.something",
-    "Incorrect project ID": "page.profile.dialog.delete.server.incorrect",
-    "Project not found": "page.profile.dialog.delete.server.notfound",
-    "No data provided for update": "page.profile.dialog.edit.server.nodata",
-    "Project name not match requirements": "page.profile.dialog.edit.server.requirements",
-    "Username not match requirements": "page.profile.dialog.user.edit.server.userrequirements",
-    "Password not match requirements": "page.profile.dialog.user.edit.server.passrequirements",
+    "Incorrect username or password": "server.incorrect",
+    "Username or password not match requirements": "server.notmatchrequirements",
+    "User created": "server.created",
+    "Username already exist": "server.exist",
+    "Project ID must be number": "server.incorrectid",
+    "User not found": "server.usernotfound",
+    "Project name can't be empty": "server.empty",
+    "Project name doesn't match requirements": "server.notmatch",
+    "Something went wrong": "server.something",
+    "Incorrect project ID": "server.incorrectprojectid",
+    "Project not found": "server.notfound",
+    "No data provided for update": "server.nodata",
+    "Project name not match requirements": "server.requirements",
+    "Username not match requirements": "server.userrequirements",
+    "Password not match requirements": "server.passrequirements",
 };
 
 export const getTranslateKey = (message: string): string => {
